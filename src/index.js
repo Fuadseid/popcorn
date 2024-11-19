b543610a13ff2400ceed0ea36c69d627b93676dd
Fuadseid <fuaddbus@gmail.com>
2024-11-19 13:22:08 +0300

    my pop corn web

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+ import './index.css';
+ import App from './App';
+/* import { StarRating } from "./StarRating";
+ */const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
+/* function Test() {
+  const [rating, onSetrating] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <>
+      <StarRating
+        maxrate={3}
+        color={"red"}
+        size={35}
+        className=""
+        message={["Bad", "good", "Excellent"]}
+        defaultRating={2}
+        onSetrating={onSetrating}
+      />
+      <p>You have rated {rating}</p>
+    </>
+  );
+} */
